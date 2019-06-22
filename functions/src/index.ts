40415873610a77457e842925fd6912f1520c0a7d
Jason Efstathiou <jason.efst@gmail.com>
2019-06-23 00:09:31 +0200

    add cancelJob endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import createChat from './chat/createChat'
 import notifier from './chat/notifier'
 import takeJob from './job/takeJob'
 import dropJob from './job/dropJob'
+import cancelJob from './job/cancelJob'
 import createProfile from './profile/createProfile'
 
 module.exports = {
@@ -11,5 +12,6 @@ module.exports = {
   createChat: functions.region('europe-west1').https.onRequest(createChat),
   notifier: functions.region('europe-west1').firestore.document('jobs/{jobId}/chat/{messageId}').onCreate(notifier),
   takeJob: functions.region('europe-west1').https.onRequest(takeJob),
+  cancelJob: functions.region('europe-west1').https.onRequest(cancelJob),
   dropJob: functions.region('europe-west1').https.onRequest(dropJob),
 }
---
+++ functions/src/job/cancelJob.ts
@@ -0,0 +1,69 @@
+const cors = require('cors')({origin: true})
+import { db } from '../admin'
+import { sendNotificationToUser } from '../utils/notify'
+import { Request, Response } from 'firebase-functions'
+
+const sendNotificationToAssignee = async (
+  assigneeId: string,
+  ownerId: string,
+  thing: string,
+) => {
+  console.log(`sending notification to ${assigneeId}`)
+  const ownerUserInfo = (await db
+    .collection('users')
+    .doc(ownerId)
+    .get())
+    .data()
+  
+  await sendNotificationToUser(ownerId, {
+    title: `${ownerUserInfo!.displayName} just cancelled their request`,
+    body: `They no longer want "${thing}".`
+  })
+}
+
+export default (req: Request, res: Response) => {
+  return cors(req, res, async () => {
+    const { jobID, uid } = req.body.data
+    const jobRef = db.collection('jobs').doc(jobID)
+    const jobSnapshot = await jobRef.get()
+
+    if (!jobSnapshot.exists) {
+      return res.status(400).send({
+        code: 'job/unknown',
+        message: 'This job does not exist'
+      })
+    }
+
+    const jobData = jobSnapshot.data()!
+
+    if (jobData.state === 'cancelled') {
+      return res.status(400).send({
+        code: 'job/already-cancelled',
+        message: 'This job is already cancelled'
+      })
+    }
+
+    if (jobData.owner !== uid) {
+      return res.status(400).send({
+        code: 'job/illegal-action',
+        message: 'This is not your job'
+      })
+    }
+
+    await jobRef.update({
+      state: 'cancelled',
+    })
+
+    if (jobData.state === 'assigned') {
+      sendNotificationToAssignee(
+        jobData.assignee.uid,
+        jobData.owner.uid,
+        jobData.thing
+        ).catch((e) => {
+          console.error(e)
+        })
+    }
+
+    return res.send({ data: { status: 'OK' }}) 
+  })
+}
