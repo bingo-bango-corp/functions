289229f3a3994c82c5ac36db4453d47efdf538e6
Jason Efstathiou <jason.efst@gmail.com>
2019-06-24 10:45:31 +0200

    allow sending type

--- functions/src/utils/notify.ts
+++ functions/src/utils/notify.ts
@@ -28,13 +28,15 @@ const getTokenForUid = async (uid: string) => {
 const sendNotificationToUser = async (uid: string, message: BingoMessagePayload) => {
   const messageToken = await getTokenForUid(uid)
   const url = message.url ? message.url : 'https://app.bingobango.app/'
+  const type = message.type ? message.type : 'generic'
   console.info(`dispatching notification to ${uid}`)
   return messaging.send({
     token: messageToken,
     data: {
       title: message.title,
       body: message.body,
-      link: url
+      link: url,
+      type: type
     }
   })
 }
